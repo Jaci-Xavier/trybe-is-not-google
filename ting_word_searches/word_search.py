595540cdb919e01b4ec3852c1a3ff639b29ffaab
Jaci-Xavier <devxvr@gmail.com>
2023-09-28 17:29:23 -0300

    req 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        occurrences = []
+        file = instance.search(i)
+
+        for line_num, line in enumerate(file
+                                        ['linhas_do_arquivo'], start=1):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": line_num})
+
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file['nome_do_arquivo'],
+                "ocorrencias": occurrences
+            })
+    return result
 
 
 def search_by_word(word, instance):
