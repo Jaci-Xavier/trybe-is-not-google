d044a01c7d32356a7928638351ef30ade9794f72
Jaci-Xavier <devxvr@gmail.com>
2023-09-28 15:22:05 -0300

    req 05

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -30,4 +30,10 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position < 0 or position >= len(instance):
+        sys.stderr.write('Posição inválida\n')
+        return None
+
+    file_return = instance.search(position)
+    sys.stdout.write(f'{file_return}\n')
+    return file_return
