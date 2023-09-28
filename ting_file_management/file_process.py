e99e270e7c920a9e928cf2b09e268c520129b35e
Jaci-Xavier <devxvr@gmail.com>
2023-09-28 15:06:56 -0300

    req 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)['nome_do_arquivo'] == path_file:
+            return None
+    file_dir = txt_importer(path_file)
+    file_return = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(file_dir),
+        'linhas_do_arquivo': file_dir
+    }
+    instance.enqueue(file_return)
+    sys.stdout.write(f'{file_return}\n')
+    return file_return
 
 
 def remove(instance):
